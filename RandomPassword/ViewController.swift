d8ebc508419389d15d61a18a292bd74e296f5533
NIKOLAI <borisov_n_88@mail.ru>
2021-01-08 16:42:36 +0300

    PasswordApp

--- RandomPassword/ViewController.swift
+++ RandomPassword/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //    let symbols = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+    
+    let length = 8
+    let pswdChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    
+    @IBOutlet weak var passwordTF: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func passwordButtonPressed(_ sender: UIButton) {
+        
+        //        let one = symbols.randomElement()
+        //        let two = symbols.randomElement()
+        //        let three = symbols.randomElement()
+        //        let four = symbols.randomElement()
+        //        let five = symbols.randomElement()
+        //        let six = symbols.randomElement()
+        //
+        //        passwordTF.text = one! + two! + three! + four! + five! + six!
+        
+        passwordTF.text = String((0..<length).compactMap{ _ in pswdChars.randomElement() })
+    }
+    
 }
 
