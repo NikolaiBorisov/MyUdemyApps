a82fe132d24edf7a19026928766b34cf50af233c
NIKOLAI <borisov_n_88@mail.ru>
2021-01-14 13:45:44 +0300

    Initial Commit

---
+++ SplitTipsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SplitTipsApp
+//
+//  Created by NIKOLAI BORISOV on 14.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
